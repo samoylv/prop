636204713a12e53518547363e527f1d2effadc4e
samoylv <xljuba@gmail.com>
2014-05-13 23:03:28 +0200

    Update propagate.py
    intensity normalization in accordance with the pixel size

--- propagate.py
+++ propagate.py
@@ -61,7 +61,7 @@ def propagate(in_fname, out_fname):
     wf.params.Mesh.xMax=wf.params.Mesh.xMax*1.e-4
     wf.params.Mesh.yMin=wf.params.Mesh.yMin*1.e-4
     wf.params.Mesh.yMax=wf.params.Mesh.yMax*1.e-4
-
+    wf.data.arrEver=arrEver*1e4
 
     print('Saving the wavefront data after propagating:'+out_fname)
     wf.store_hdf5(out_fname)
