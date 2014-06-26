f42217e04104997a95d7e495d038e73f1327b5fa
Alexey Buzmakov <buzmakov@gmail.com>
2014-06-26 15:12:48 +0400

    update for focusing to 200 nm FWHM

--- propagate.py
+++ propagate.py
@@ -93,17 +93,17 @@ def propagate(in_fname, out_fname):
     wf=Wavefront()
     wf.load_hdf5(in_fname)
     distance = 300.
-    foc_dist = 10.
+    foc_dist = 2.
     drift0 = wpg.optical_elements.Drift(distance)
     lens0  = wpg.optical_elements.Lens(foc_dist, foc_dist)
-    drift1 = wpg.optical_elements.Drift(1./(1./foc_dist - 1./distance))
+    drift1 = wpg.optical_elements.Drift(1./(1./foc_dist-1./distance))
     ##srwl_bl0 = SRWLOptC([drift0, ], [Use_PP(semi_analytical_treatment=1,zoom=0.1,sampling=4)])
     ##bl0 = wpg.Beamline(srwl_bl0)
 
     bl0 = wpg.Beamline()
     bl0.append(drift0, Use_PP(semi_analytical_treatment=1, zoom=0.15, sampling=8))
     bl0.append(lens0,  Use_PP())
-    bl0.append(drift1, Use_PP(semi_analytical_treatment=1, zoom=2.5, sampling=0.4))
+    bl0.append(drift1, Use_PP(semi_analytical_treatment=1, zoom=4.2,  sampling=0.5))
     
     if isIpynb:
         print bl0
