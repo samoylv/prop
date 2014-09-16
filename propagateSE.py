760c3b21deaefdd7b47d5028b987a37e7fdef83b
lsamoylv <lsamoylv@wla000632wpa.desy.de>
2014-09-16 12:27:20 +0200

    minor corrections

--- propagateSE.py
+++ propagateSE.py
@@ -55,7 +55,7 @@ def mkdir_p(path):
 
 # In[ ]:
 
-def defineOPD(opTrErMirr, mdatafile, ncol, delim, orient, theta, scale):
+def defineOPD(opTrErMirr, mdatafile, ncol, delim, orient, theta, scale=1., stretching=1.):
     """
     Define optical path difference (OPD) from mirror profile
     
@@ -65,9 +65,11 @@ def defineOPD(opTrErMirr, mdatafile, ncol, delim, orient, theta, scale):
     :param delim: delimiter between numbers in an row, can be space (' '), tab '\t', etc
     :param orient: mirror orientation, 'x' (horizontal) or 'y' (vertical)
     :param theta: mirror incidence angle
-    :param scale: scaling factor for the mirror profile
+    :param scale: scaling factor for the mirror profile height errors
+    :param stretching: scaling factor for the mirror profile x-axis (a hack, should be removed ASAP) 
     """
-    heightProfData = np.loadtxt(mdatafile).T
+    heightProfData = numpy.loadtxt(mdatafile).T
+    heightProfData[0,:] = heightProfData[0,:] * stretching
     wpg.useful_code.srwutils.AuxTransmAddSurfHeightProfileScaled(opTrErMirr, heightProfData, orient, theta, scale)
     if isIpynb:
         pylab.figure(); pylab.plot(heightProfData[0],heightProfData[ncol-1]*1e9)
@@ -178,28 +180,28 @@ def propagate(in_fname, out_fname):
     drift_to_foc = wpg.optical_elements.Drift(distance_foc)
     
     om_mirror_length = 0.8; om_clear_ap = om_mirror_length*theta_om
-    kb_mirror_length = 0.8; kb_clear_ap = kb_mirror_length*theta_kb
+    kb_mirror_length = 0.9; kb_clear_ap = kb_mirror_length*theta_kb
     ap0   = wpg.optical_elements.Aperture('r','a', 120.e-6, 120.e-6)
     ap1   = wpg.optical_elements.Aperture('r','a', om_clear_ap, 2*om_clear_ap)
     ap_kb = wpg.optical_elements.Aperture('r','a', kb_clear_ap, kb_clear_ap)
     hfm    = wpg.optical_elements.Mirror_elliptical(_p=distance, _q=(distance_hfm_vfm+distance_foc), _ang_graz=theta_kb,                                                     _r_sag=1.e+40, _size_tang=0.9, _nvx=np.cos(theta_kb),  _nvy=0,                                                     _nvz=-np.sin(theta_kb), _tvx=-np.sin(theta_kb), _tvy=0, _x=0, _y=0,                                                     _treat_in_out=1)     
     vfm    = wpg.optical_elements.Mirror_elliptical(_p=(distance+distance_hfm_vfm), _q=distance_foc, _ang_graz=theta_kb,                                                     _r_sag=1.e+40, _size_tang=0.9, _nvx=0, _nvy=np.cos(theta_kb),                                                     _nvz=-np.sin(theta_kb), _tvx=0, _tvy=-np.sin(theta_kb), _x=0, _y=0,                                                     _treat_in_out=1) 
     wf_dist_om = wpg.optical_elements.WF_dist(1500, 100, om_clear_ap, 2*om_clear_ap)
-    defineOPD(wf_dist_om, os.path.join(mirror_data_dir,'mirror2.dat'), 2, '\t', 'x',                theta_kb, scale=4)
+    defineOPD(wf_dist_om, os.path.join(mirror_data_dir,'mirror2.dat'), 2, '\t', 'x',                theta_kb, scale=2)
     if isIpynb:
         meshT = wf_dist_om.mesh
         opdTmp=numpy.array(wf_dist_om.arTr)[1::2].reshape(meshT.ny,meshT.nx)
         figure(); pylab.imshow(opdTmp,extent=[meshT.xStart,meshT.xFin,meshT.yStart,meshT.yFin])
         pylab.title('OPD [m]');pylab.xlabel('x (m)'); pylab.ylabel('y (m)')  
     wf_dist_hfm = wpg.optical_elements.WF_dist(1500, 100, kb_clear_ap, kb_clear_ap)
-    defineOPD(wf_dist_hfm, os.path.join(mirror_data_dir,'mirror1.dat'), 2, '\t', 'x',  theta_kb, scale=2)
+    defineOPD(wf_dist_hfm, os.path.join(mirror_data_dir,'mirror1.dat'), 2, '\t', 'x',                theta_kb, scale=2, stretching=kb_mirror_length/0.8)
     if isIpynb:
         meshT = wf_dist_hfm.mesh
         opdTmp=np.array(wf_dist_hfm.arTr)[1::2].reshape(meshT.ny,meshT.nx)
         figure(); pylab.imshow(opdTmp,extent=[meshT.xStart,meshT.xFin,meshT.yStart,meshT.yFin])
         pylab.title('OPD [m]');pylab.xlabel('x (m)'); pylab.ylabel('y (m)')  
     wf_dist_vfm = wpg.optical_elements.WF_dist(1100, 1500, kb_clear_ap, kb_clear_ap)
-    defineOPD(wf_dist_vfm, os.path.join(mirror_data_dir,'mirror2.dat'), 2, ' ', 'y',  theta_kb, scale=2)
+    defineOPD(wf_dist_vfm, os.path.join(mirror_data_dir,'mirror2.dat'), 2, ' ', 'y',                theta_kb, scale=2, stretching=kb_mirror_length/0.8)
     if isIpynb:
         meshT = wf_dist_vfm.mesh
         opdTmp=np.array(wf_dist_vfm.arTr)[1::2].reshape(meshT.ny,meshT.nx)
@@ -232,7 +234,26 @@ def propagate(in_fname, out_fname):
     sz1 = get_intensity_on_axis(wf);
     wf.custom_fields['/misc/spectrum1'] = sz1
     
-    wpg.srwlib.srwl.SetRepresElecField(wf._srwl_wf, 't')
+    wpg.srwlib.srwl.SetRepresElecField(wf._srwl_wf, 't')def defineOPD(opTrErMirr, mdatafile, ncol, delim, orient, theta, scale=1., stretching=1.):
+    """
+    Define optical path difference (OPD) from mirror profile
+    
+    :param opTrErMirr: the struct with wave front distortions from mirror susrface errors
+    :param mdatafile: an ascii file with mirror profile data
+    :param ncol: number of columns in the mirror profile file
+    :param delim: delimiter between numbers in an row, can be space (' '), tab '\t', etc
+    :param orient: mirror orientation, 'x' (horizontal) or 'y' (vertical)
+    :param theta: mirror incidence angle
+    :param scale: scaling factor for the mirror profile height errors
+    :param stretching: scaling factor for the mirror profile x-axis (a hack, should be removed ASAP) 
+    """
+    heightProfData = numpy.loadtxt(mdatafile).T
+    heightProfData[0,:] = heightProfData[0,:] * stretching
+    wpg.useful_code.srwutils.AuxTransmAddSurfHeightProfileScaled(opTrErMirr, heightProfData, orient, theta, scale)
+    if isIpynb:
+        pylab.figure(); pylab.plot(heightProfData[0],heightProfData[ncol-1]*1e9)
+        pylab.title('profile from %s' %mdatafile);pylab.xlabel('x (m)');pylab.ylabel('h (nm)') 
+
 
     #Resizing: decreasing Range of Horizontal and Vertical Position:
     wpg.srwlib.srwl.ResizeElecField(wf._srwl_wf, 'c', [0, 0.25, 1, 0.25,  1]);
